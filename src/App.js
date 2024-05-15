dd7a1ae73a687d0a5a8dafa967f60566cf2a359b
zaryab ikram <zaryabsheikh0@gmail.com>
2024-05-15 19:12:20 +0500

    changein colorboxex

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { boxes } from "./components/Box";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [isclicked, setisClicked] = useState(false);
+  const [allBox, setAllBox] = useState(boxes);
+
+  const [sequance, setSequance] = useState([]);
+
+  const handleClick = (e, box, i) => {
+    if (!box.color) setSequance([...sequance, i]);
+
+    let newbox = allBox.map((b, i) => {
+      if (b.id === box.id) {
+        return { id: b.id, color: !b.color };
+      }
+      return b;
+    });
+
+    setAllBox(newbox);
+  };
+
+  useEffect(() => {
+    const filterdArr = [];
+    allBox.forEach((f) => {
+      if (f.color) return filterdArr.push(f);
+    });
+
+    let everyfunc = allBox.every((e) => {
+      return e.color;
+    });
+
+    const updateItems = (e, index) => {
+      setTimeout(() => {
+        console.log(sequance);
+        setSequance((seq) => seq?.slice(0, seq.length - 1));
+      }, 1000 * index); // Delay each update by 1 second multiplied by the index
+      setAllBox(boxes);
+    };
+    if (everyfunc) {
+      allBox.forEach((e, i) => {
+        updateItems(e, i);
+      });
+    }
+  }, [allBox]);
+
+  //    if (everyfunc) {
+  //     setSequance(true)
+  //      setAllBox(() => filterdArr);
+
+  //       filterdArr.forEach((f) => {
+  //         setTimeout(() => {
+  //           setAllBox((prevItems) => {
+  //             const newItems = [...prevItems];
+  //             f.color = false;
+  //             return newItems;
+  //           });
+  //         }, 1000); // Delay each update by 1 second multiplied by the index
+  //       });
+  //    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="main">
+      {allBox?.map((box, i) => (
+        <div
+          style={{
+            ...(sequance?.includes(i) && { backgroundColor: "red" }),
+          }}
+          className="box"
+          onClick={(e) => handleClick(e, box, i)}
         >
-          Learn React
-        </a>
-      </header>
+          {""}
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Box.js
@@ -0,0 +1,8 @@
+export const boxes = [
+  { id: 1, color: false },
+  { id: 2, color: false },
+  { id: 3, color: false },
+  { id: 4, color: false },
+  { id: 5, color: false },
+  { id: 6, color: false },
+];
